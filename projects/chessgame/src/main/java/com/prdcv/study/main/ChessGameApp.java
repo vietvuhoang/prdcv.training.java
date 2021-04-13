a408b291476b9eccd38d106f9a2bf2f4a06434f2
Viet Vu <vietvuhoang@gmail.com>
2021-04-13 22:52:42 +0700

    Chess Game added

---
+++ projects/chessgame/src/main/java/com/prdcv/study/main/ChessGameApp.java
@@ -0,0 +1,13 @@
+package com.prdcv.study.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class ChessGameApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
